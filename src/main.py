46bf6e723191c7f2641bd0823e96c661156d02a6
Carlos Magalhães Silva <carlosmsilva755@gmail.com>
2021-08-28 18:11:30 -0300

    implementing set cover

--- src/main.py
+++ src/main.py
@@ -1,16 +1,50 @@
 import numpy as np
-# n(linhas) vai ser o numero de pontos
-# m(colunas) vai ser o numero de subconjs
-# Ci é o peso do subconj i
-def getInput():
-  n, m = map(int, input().split())
-  c = np.array([input().strip().split()], int)#pegando o c
-  matrix = np.array([input().strip().split() for _ in range(n)], int)#matriz
 
-  return n, m,  matrix, c
+def getInput():
+  
+  n, m = map(int, input().split())
+  c = np.array([input().strip().split()], int)
+  matriz = np.array([input().strip().split() for _ in range(n)], int)
+
+  return n, m, matriz, c
 
 def main():
 
-	n, m , matrix, c = getInput()
-	
+  n, m , matriz, c = getInput()
+
+  pesos = c[0]
+  pontos = np.zeros(n, int)
+  sc = np.zeros(m, int)
+
+  menor = 1000000
+  indexMenor = -1
+  estaCoberto = False
+
+  for i in range(n):
+    for j in range(m):
+
+      if matriz[i][j] == 1:
+        if sc[j] == 1:
+          estaCoberto = True
+        elif sc[j] == 0 and pesos[j] < menor:
+          indexMenor = j
+          menor = pesos[j]
+
+    else:
+      if not estaCoberto:
+        for k in range(m):
+          if matriz[i][k] == 1:
+            pesos[k] -= menor
+      
+        sc[indexMenor] = 1 # colocando subconj na solucao
+        pontos[i] = menor # peso
+
+      menor = 1000000
+      indexMenor = -1
+      estaCoberto = False
+
+  print()
+  print(*sc)
+  print(*pontos)
+
 main()
